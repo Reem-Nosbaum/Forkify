46a62a816040b0630e17f5937daf4eae311605bc
Reem-Nosbaum <reemnu@gmail.com>
2023-07-30 15:58:57 +0300

    Improve structure / format of the code. :art:

--- src/js/controller.js
+++ src/js/controller.js
@@ -37,19 +37,24 @@ const controlSearchResults = async function() {
     if(!query) return
 
    await model.loadSearchResults(query)
-  //  resultsView.render(model.state.search.results)
-  resultsView.render(model.getSearchResultsPage(4))
 
-    paginationView.render(model.state.search)
+   resultsView.render(model.getSearchResultsPage())
 
+   paginationView.render(model.state.search)
   } catch(err) {
     console.log(err) 
   }
 }
 
+const controlPagination = function(goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage))
+
+    paginationView.render(model.state.search)
+}
 
 const init = function() {
   recpieView.addHandlerRender(controlRecipes)
   searchView.addHandlerSearch(controlSearchResults)
+  paginationView.addHandlerClick(controlPagination)
 }
 init()
--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -4,6 +4,17 @@ import icons from '../../img/icons.svg'
 class PaginationView extends View{
     _parentElement = document.querySelector('.pagination')
 
+    addHandlerClick(handler) {
+        this._parentElement.addEventListener('click', function(e){
+            const btn = e.target.closest('.btn--inline')
+            if(!btn) return
+
+            const goToPage = +btn.dataset.goto
+            console.log(goToPage)
+            handler(goToPage)
+        })
+    }
+
     _generateMarkup() {
         const curPage = this._data.page
         const numPages = Math.ceil(
@@ -13,7 +24,7 @@ class PaginationView extends View{
         // page 1, and there are other pages
         if(curPage === 1 && numPages > 1)
         return `
-            <button class="btn--inline pagination__btn--next">
+            <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
                 <span>Page ${curPage + 1}</span>
                 <svg class="search__icon">
                 <use href="${icons}#icon-arrow-right"></use>
@@ -24,7 +35,7 @@ class PaginationView extends View{
 
         if(curPage === numPages && numPages > 1) {
             return `
-                <button class="btn--inline pagination__btn--prev">
+                <button data-goto="${curPage - 1}" class="btn--inline pagination__btn--prev">
                     <svg class="search__icon">
                     <use href="${icons}#icon-arrow-left"></use>
                     </svg>
@@ -37,14 +48,14 @@ class PaginationView extends View{
 
         if(curPage < numPages) {
             return `
-            <button class="btn--inline pagination__btn--prev">
+            <button data-goto="${curPage - 1}" class="btn--inline pagination__btn--prev">
             <svg class="search__icon">
             <use href="${icons}#icon-arrow-left"></use>
             </svg>
             <span>Page ${curPage - 1}</span>
             </button>
 
-            <button class="btn--inline pagination__btn--next">
+            <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
             <span>Page ${curPage + 1}</span>
             <svg class="search__icon">
             <use href="${icons}#icon-arrow-right"></use>
